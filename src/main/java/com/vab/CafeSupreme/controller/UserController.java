3c6594f34da187891db64ddea47fa45f452d69c4
unknown <vaibhavvergil3@gmail.com>
2025-07-03 03:19:07 +0530

    Update

--- src/main/java/com/vab/CafeSupreme/controller/UserController.java
+++ src/main/java/com/vab/CafeSupreme/controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/user")
@@ -23,17 +25,16 @@ public class UserController {
 
     @GetMapping("/register")
     public String registerUser(Model model) {
+
+        if (userService.getLoggedInUser() != null)
+            return "redirect:/home";
+
         model.addAttribute("userDto", new UserDTO());
         return "register.html";
     }
 
-    @GetMapping("/login")
-    public String loginUser() {
-        return "login.html";
-    }
-
     @PostMapping("/register")
-    public String registerUserPost(Model model, @ModelAttribute("userDto") UserDTO userDTO) {
+    public String registerUserPost(Model model, @ModelAttribute("userDto") UserDTO userDTO, BindingResult result, RedirectAttributes redirectAttributes) {
         UserDetails userDetails = new UserDetails();
         userDetails.setFirstName(userDTO.getFirstName());
         userDetails.setLastName(userDTO.getLastName());
@@ -43,6 +44,17 @@ public class UserController {
         userDetails.setRole(UserRole.ROLE_USER.name());
         userDetails.setMobileNumber(userDTO.getMobileNumber());
         userService.addUser(userDetails);
-        return "registrationsuccessful.html";
+
+        redirectAttributes.addFlashAttribute("message", "success");
+        return "redirect:/user/login";
+    }
+
+    @GetMapping("/login")
+    public String loginUser(Model model, @ModelAttribute("message") String message) {
+        if (userService.getLoggedInUser() != null)
+            return "redirect:/home";
+
+        model.addAttribute("message", message);
+        return "login.html";
     }
 }
--- src/main/java/com/vab/CafeSupreme/service/UserService.java
+++ src/main/java/com/vab/CafeSupreme/service/UserService.java
@@ -2,6 +2,7 @@ package com.vab.CafeSupreme.service;
 
 import com.vab.CafeSupreme.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import com.vab.CafeSupreme.entity.UserDetails;
 
@@ -12,6 +13,11 @@ public class UserService {
     public void addUser(UserDetails userDetails)
     {
         userRepository.save(userDetails);
+    }
+
+    public UserDetails getLoggedInUser()
+    {
+       return userRepository.getUserDetailsByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
     }
 
 }
